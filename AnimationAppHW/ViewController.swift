fb5eede074370e0eae1eebda1c985672f75ceaf6
Anton Zarichny <antonzarichny@gmail.com>
2022-09-13 19:07:35 +0300

    Initial Commit

---
+++ AnimationAppHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationAppHW
+//
+//  Created by Антон Заричный on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
